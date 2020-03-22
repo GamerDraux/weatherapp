a86d64b0a751ab72135ce1a65f4ccdd017797cdb
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-22 11:54:18 -0500

    trying to add a kelvin/farenheit module

---
+++ kelvintofarenheit.js
@@ -0,0 +1,6 @@
+let kelvinToFarenheit = function(kelvin){
+    let f=(kelvin-273.15)*9/5+32;
+    return f;
+}
+
+module.exports= kelvinToFarenheit;
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+const kelvinToFarenheit = require('./kelvintofarenheit');
+
 window.addEventListener('load', function (){
     console.log ('window loaded');
     defineElementVariables();
-
+    let weatherJson= {};
+    getJsonInfo();
 })
 
 
@@ -17,4 +20,22 @@ function defineElementVariables (){
     // console.log ('precipitationBox variable set to: '+precipitationBox);
     let bigBox=document.getElementById('bigBox');
     // console.log ('bigBox variable set to : '+bigBox);
+}
+
+function getJsonInfo(){
+    console.log ('getJsonInfo fired');
+    const responsePromise=fetch('http://api.openweathermap.org/data/2.5/weather?zip=63385,us&appid=e770ede1e51fbbb66b830c2986e6bb7a');
+    responsePromise.then(function(response){
+        const jsonPromise=response.json();
+        jsonPromise.then(function(json){
+            weatherJson=json.main;
+            console.log(weatherJson);
+        })
+    })
+    
+}
+
+function resetDisplay(weatherJson){;
+    console.log ('resetDisplay fired');
+    console.log (weatherJson);
 }
